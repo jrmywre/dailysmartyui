982911c86b9bc5499bc2984ef55bff7279f6d8b0
jrmywre <jrmywre@gmail.com>
2020-02-19 13:40:30 -0500

    created post component

---
+++ src/components/posts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render (){
+        return (
+        <li>
+            {postMessage.title}
+        </li>
+        )
+    }
+}
+
+export default Post
--- src/components/recentposts.js
+++ src/components/recentposts.js
@@ -14,9 +14,7 @@ class RecentPosts extends Component {
         const posts = this.props.recentPosts.map((post, index) => {
             if(index < 3) {
                 return (
-                    <li key={index}>
-                        {post.title}
-                    </li>
+                    <Post {...post} key={title} />
                 )   
             }
         })
